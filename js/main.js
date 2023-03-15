397f9867e1d455e720fd106e21d0bde0f5628d97
LEV <markovchik2003@yandex.ru>
2023-03-15 18:59:08 +0300

    done

--- js/main.js
+++ js/main.js
@@ -14,3 +14,22 @@ function checkLength(string, length) {
   if (length < 0) return 0;
   return string.length <= length;
 }
+
+let usedIDs = [];
+
+const createPhoto = () => {
+  let i = getRandomInt(1, 25);
+  while (usedIDs.indexOf(i) != -1) {
+    i = getRandomInt(1, 25);
+  }
+  usedIDs.push(i);
+  return {
+    id: i,
+    url: "photos/${i}.jpg",
+    description: "Photo number ${i}",
+    likes: getRandomInt(15, 200),
+    comments: getRandomInt(0, 200),
+  }
+}
+
+const photos = Array.from({length: 25}, createPhoto);
