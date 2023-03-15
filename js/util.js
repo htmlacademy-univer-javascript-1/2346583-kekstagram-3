431ccb5462ad3656bb87a0cf5a6949bf949e0062
LEV <markovchik2003@yandex.ru>
2023-03-15 19:20:19 +0300

    done

---
+++ js/util.js
@@ -0,0 +1,20 @@
+export function getRandomInt(min, max) { //from https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+  if (min > max || min < 0 || max < 0){
+    return 0;
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (min === max) {
+    return min;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function checkLength(string, length) {
+  length = Number(length);
+  string = String(string);
+  if (length < 0) {
+    return 0;
+  }
+  return string.length <= length;
+}
