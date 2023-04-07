efa4ad5d69118d1ae44747baf3bba5ee40d159aa
LEV <markovchik2003@yandex.ru>
2023-04-07 21:05:52 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -3,5 +3,4 @@ import { displayData } from "./displayData.js";
 import './uploadPicture.js';
 
 const photos = Array.from({length: 25}, createPhoto);
-photos.forEach((i) => {console.log(i.url)});
 displayData(photos);
