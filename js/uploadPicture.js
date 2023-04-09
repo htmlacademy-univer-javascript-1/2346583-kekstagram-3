2bfb6870b77035abaaa6440ca30add3bcb405755
LEV <markovchik2003@yandex.ru>
2023-04-09 17:42:57 +0300

    fix

--- js/uploadPicture.js
+++ js/uploadPicture.js
@@ -1,10 +1,13 @@
 import {pristine} from "./validation.js";
 const pictureUploadForm = document.querySelector('#upload-select-image');
-
+const preview = document.querySelector('.img-upload__preview img');
 const pictureUploadOverlay = pictureUploadForm.querySelector('.img-upload__overlay');
-
+var onEditorEscKeydown;
 const pictureInput = pictureUploadForm.querySelector('#upload-file');
 const overlayCloseButton = pictureUploadForm.querySelector('#upload-cancel');
+const effects = editor.querySelector('.effects__list');
+const scaleSmallerButton = editor.querySelector('.scale__control--smaller');
+const scaleBiggerButton = editor.querySelector('.scale__control--bigger');
 
 function closeImageUploadModal() {
   pictureUploadOverlay.classList.add('hidden');
@@ -16,6 +19,20 @@ function closeImageUploadModal() {
 pictureInput.addEventListener('change', function(evt) {
   pictureUploadOverlay.classList.toggle('hidden');
   document.body.classList.toggle('modal-open');
+  const uploadedImage =  document.querySelector('#upload-file').files[0];
+
+  const fileReader = new FileReader();
+  createSlider();
+
+  fileReader.onloadend = function() {
+    preview.src = fileReader.result;
+  };
+  fileReader.readAsDataURL(uploadedImage);
+
+  onEditorEscKeydown = escKeydownHandler(document, closeEditor);
+  effects.addEventListener('change', onEffectButtonClick);
+  scaleSmallerButton.addEventListener('click', onControlSmallerButtonClick);
+  scaleBiggerButton.addEventListener('click', onControlBiggerButtonClick);
 });
 
 // close modal
